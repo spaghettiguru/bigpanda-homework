d437bb66fa298192821831a0afa7315102c45dd1
Oleg Yuzvik <yuzvik.oleg@gmail.com>
2019-04-09 22:28:57 +0300

    removed some semi-colons and a route param

--- back-end/server.js
+++ back-end/server.js
@@ -7,22 +7,25 @@ const PORT = 3001
 app.use(cors())
 
 app.get('/', (req, res) => {
-    const email = req.query.email;
+    const email = req.query.email
 
     const commentsMock = [
         {userID: 'yuzvik.oleg@gmail.com', text: 'No comments.', userPicURL: 'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50'},
         {userID: 'john.doe@mail.ru', text: 'There is a very long comment here.', userPicURL: 'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50'},
         {userID: 'fakemail@fake.com', text: 'My email is fake, so is this comment.', userPicURL: 'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50'}
     ]
-    let commentsToReturn;
+    let commentsToReturn
     if (email) {
-        commentsToReturn = commentsMock.filter(comment => comment.userID.toLowerCase() === email.toLowerCase());
+        commentsToReturn = commentsMock.filter(comment => comment.userID.toLowerCase() === email.toLowerCase())
     } else {
-        commentsToReturn = commentsMock;
+        commentsToReturn = commentsMock
     }
 
     res.send(commentsToReturn)
 })
-app.post('/:id', (req, res) => {})
+
+app.post('/', (req, res) => {
+    // TODO: implement create comment
+})
 
 app.listen(PORT, () => console.log(`Comments service is listening on port ${PORT}!`))
