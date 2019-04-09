00ab031f809a5c7148a9e6b4a701540579ef0547
Oleg Yuzvik <yuzvik.oleg@gmail.com>
2019-04-09 20:37:30 +0300

    calling real API

--- front-end/src/components/CommentsSection/CommentsSection.js
+++ front-end/src/components/CommentsSection/CommentsSection.js
@@ -19,12 +19,10 @@ export class CommentsSection extends Component {
     }
 
     async componentDidMount() {
+        
+        // TODO: extract comments fetching to the separate method
         try {
-            const comments  = await Promise.resolve([
-                {userID: 'yuzvik.oleg@gmail.com', text: 'No comments.', userPicURL: 'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50'},
-                {userID: 'john.doe@mail.ru', text: 'There is a very long comment here.', userPicURL: 'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50'},
-                {userID: 'fakemail@fake.com', text: 'My email is fake, so is this comment.', userPicURL: 'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50'}
-            ]);
+            const comments  = await fetch('http://localhost:3001/').then(response => response.json());
             this.setState({
                 comments
             });
