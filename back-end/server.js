6c7a5a4f94912d918829bb005d459f3a250a439e
Oleg Yuzvik <yuzvik.oleg@gmail.com>
2019-04-09 22:20:09 +0300

    implemented mock comment filtering

--- back-end/server.js
+++ back-end/server.js
@@ -7,12 +7,21 @@ const PORT = 3001
 app.use(cors())
 
 app.get('/', (req, res) => {
-    
-    res.send([
+    const email = req.query.email;
+
+    const commentsMock = [
         {userID: 'yuzvik.oleg@gmail.com', text: 'No comments.', userPicURL: 'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50'},
         {userID: 'john.doe@mail.ru', text: 'There is a very long comment here.', userPicURL: 'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50'},
         {userID: 'fakemail@fake.com', text: 'My email is fake, so is this comment.', userPicURL: 'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50'}
-    ]);
+    ]
+    let commentsToReturn;
+    if (email) {
+        commentsToReturn = commentsMock.filter(comment => comment.userID.toLowerCase() === email.toLowerCase());
+    } else {
+        commentsToReturn = commentsMock;
+    }
+
+    res.send(commentsToReturn)
 })
 app.post('/:id', (req, res) => {})
 
