3670de4eb80441f09f1e0d64d953a55edd3b01b9
Oleg Yuzvik <yuzvik.oleg@gmail.com>
2019-04-10 00:25:55 +0300

    comments filtering and fetch error handling

--- front-end/src/components/CommentsSection/CommentsSection.js
+++ front-end/src/components/CommentsSection/CommentsSection.js
@@ -15,7 +15,8 @@ export class CommentsSection extends Component {
         this.state = {
             filterText: '',
             comments: null,
-            commentsFetchError: false
+            commentsFetchError: false,
+            isLoadingComments: false
         };
 
         this.filterTextChanged = this.filterTextChanged.bind(this);
@@ -23,17 +24,20 @@ export class CommentsSection extends Component {
 
     async componentDidMount() {
 
-        // TODO: extract comments fetching to the separate method
+        // using single partial state object to prevent unnecessary calls to setState 
+        // (and as a result consecutive re-renders)
+        const stateToUpdate = {};
         try {
-            const comments  = await this.fetchComments();
             this.setState({
-                comments
+                isLoadingComments: true
             });
+            stateToUpdate.comments = await this.fetchComments();
         } catch(error) {
-            this.setState({
-                commentsFetchError: true
-            });
+            stateToUpdate.commentsFetchError = true;
             console.error(error.message);
+        } finally {
+            stateToUpdate.isLoadingComments = false;
+            this.setState(stateToUpdate);
         }
     }
 
@@ -51,6 +55,7 @@ export class CommentsSection extends Component {
                     {
                         this.state.comments.map(
                             comment => 
+                            // TODO: change key to be unique
                             <li className="comments-list-item" key={comment.userID}>
                                 <Comment 
                                     userPicURL={comment.userPicURL}
@@ -60,26 +65,50 @@ export class CommentsSection extends Component {
                         )
                     }
                 </ul>}
-                {!this.state.comments && !this.state.commentsFetchError && <p>Loading comments...</p>}
+                {this.state.isLoadingComments && <p className="empty-state-message">Loading comments...</p>}
+                {this.state.comments && this.state.comments.length === 0 && 
+                    <p className="empty-state-message">No comments found that satisfy the specified filter criterion.</p>}
                 {this.state.commentsFetchError && 
-                    <p>Network rrror occured during comments retrieval. Please try refreshing the page.</p>}
+                    <p className="empty-state-message">Network error occured during comments retrieval. Please try refreshing the page.</p>}
             </div>
         )
     }
 
-    filterTextChanged(e) {
+    async filterTextChanged(e) {
+        // TODO: implement debounce
+        const filterText = e.target.value;
         this.setState({
-            filterText: e.target.value
+            filterText,
+            isLoadingComments: true
         });
+
+        let filteredComments;
+        try {
+            filteredComments = await this.fetchComments(filterText);
+        } catch(error) {
+            filteredComments = null;
+            // TODO: set error flag in the state to true
+            console.error(error.message);
+        } finally {
+            this.setState({
+                comments: filteredComments,
+                isLoadingComments: false
+            });
+        }
     }
 
     postComment(comment) {
+        // TODO: implement
         console.log('posting comment: ', comment);
     }
 
     // TODO: move to the separate module
-    fetchComments(email='') {
-        return fetch(`${COMMENTS_SERVICE_BASE_URL}?email=${email}`)
+    fetchComments(email) {
+        let requestURL = COMMENTS_SERVICE_BASE_URL;
+        if (email) {
+            requestURL += '?email=' + email;
+        }
+        return fetch(requestURL)
             .then(response => response.json())
     }
 }
