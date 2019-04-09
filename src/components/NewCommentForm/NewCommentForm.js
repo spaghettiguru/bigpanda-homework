5b59d7857ba9da18eabc6dd96f44f260887b95d5
Oleg Yuzvik <yuzvik.oleg@gmail.com>
2019-04-09 16:59:44 +0300

    set both form fields to be required and bound submit handler to this

--- src/components/NewCommentForm/NewCommentForm.js
+++ src/components/NewCommentForm/NewCommentForm.js
@@ -5,13 +5,15 @@ import './NewCommentForm.scss';
 export class NewCommentForm extends Component {
     constructor(props) {
         super(props);
+
+        this.submitForm = this.submitForm.bind(this);
     }
 
     render() {
         return (
             <form onSubmit={this.submitForm} className="new-comment-form">
-                <input type="email" placeholder="Email" />
-                <textarea placeholder="Message" rows="5"></textarea>
+                <input type="email" name="email" placeholder="Email" required />
+                <textarea placeholder="Message" name="text" rows="5" required></textarea>
                 <button className="submit-comment-btn button-primary">Submit</button>
             </form>
         )
