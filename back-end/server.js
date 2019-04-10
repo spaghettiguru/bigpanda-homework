80f0c1767c4766f4dad6ee47611fac58b8a3002f
Oleg Yuzvik <yuzvik.oleg@gmail.com>
2019-04-10 23:50:56 +0300

    [SERVER] implemented post comment route

--- back-end/server.js
+++ back-end/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+var bodyParser = require('body-parser')
 
 const config = require('./config')
 const commentsActions = require('./db/actions/comments')
@@ -7,11 +8,11 @@ const commentsActions = require('./db/actions/comments')
 const app = express()
 
 app.use(cors())
+app.use(bodyParser.json())
 
 app.get('/', async (req, res) => {
     console.log('\nIncoming request with the following params: \n METHOD: %s\n URL: %s\n', req.method, req.url)
     const email = req.query.email
-
 
     // const commentsMock = [
     //     {userID: 'yuzvik.oleg@gmail.com', text: 'No comments.', userPicURL: 'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50'},
@@ -28,8 +29,12 @@ app.get('/', async (req, res) => {
     res.send(commentsToReturn)
 })
 
-app.post('/', (req, res) => {
-    // TODO: implement create comment
+app.post('/', async (req, res) => {
+    console.log('\nIncoming request with the following params: \n METHOD: %s\n URL: %s\n', req.method, req.url)
+
+    const createdComment = await commentsActions.insertSingle(req.body)
+
+    res.send(createdComment);
 })
 
 app.listen(config.httpServerPort, () => console.log(`Comments service is listening on port ${config.httpServerPort}!`))
