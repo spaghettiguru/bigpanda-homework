1c9a3999e2e84067d4e16938abab0e0232ae96e9
Oleg Yuzvik <yuzvik.oleg@gmail.com>
2019-04-10 22:21:22 +0300

    created db actions file

---
+++ back-end/db/actions.js
@@ -0,0 +1,32 @@
+const config = require('../config')
+const MongoClient = require('mongodb').MongoClient
+
+    async function fetchAll() {
+        let dbClientInstance;
+        let comments = null
+        try {
+            dbClientInstance = await MongoClient.connect(config.dbServerURL)
+            console.log("Connected successfully to MongoDB server")
+            const db = dbClientInstance.db(config.dbName)
+            const commentsCollection = db.collection('comments')
+
+            try {
+                comments = await commentsCollection.find({}).toArray()
+            } catch(error) {
+                console.error('[ERROR] Failed to convert cursor to array. Got error: ', error)
+            }
+        } catch (error) {
+            console.error('[ERROR] Failed to connect to the database. Got error: ', error)
+        } finally {
+            if (dbClientInstance) {
+                dbClientInstance.close()
+            }
+            
+            return comments
+        }
+    }
+
+    function fetchCommentsByEmail(email) {}
+    function insertComment(comment) {}
+
+module.exports = {fetchAll, fetchCommentsByEmail, insertComment}
