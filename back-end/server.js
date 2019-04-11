98cd6c60d86feecbb3aae18a81a182753184dad6
oleg.yuzvik <oleg.yuzvik@sizmek.com>
2019-04-11 16:31:25 +0300

    [SERVER] generating image URL for new comment

--- back-end/server.js
+++ back-end/server.js
@@ -1,3 +1,5 @@
+const crypto = require('crypto')
+
 const express = require('express')
 const cors = require('cors')
 var bodyParser = require('body-parser')
@@ -14,16 +16,11 @@ app.get('/', async (req, res) => {
     console.log('\nIncoming request with the following params: \n METHOD: %s\n URL: %s\n', req.method, req.url)
     const email = req.query.email
 
-    // const commentsMock = [
-    //     {userID: 'yuzvik.oleg@gmail.com', text: 'No comments.', userPicURL: 'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50'},
-    //     {userID: 'john.doe@mail.ru', text: 'There is a very long comment here.', userPicURL: 'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50'},
-    //     {userID: 'fakemail@fake.com', text: 'My email is fake, so is this comment.', userPicURL: 'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50'}
-    // ]
-    let commentsToReturn;
+    let commentsToReturn
     if (email) {
-        commentsToReturn = await commentsActions.fetchByEmail(email);
+        commentsToReturn = await commentsActions.fetchByEmail(email)
     } else {
-        commentsToReturn = await commentsActions.fetchAll();
+        commentsToReturn = await commentsActions.fetchAll()
     }
 
     res.send(commentsToReturn)
@@ -32,9 +29,15 @@ app.get('/', async (req, res) => {
 app.post('/', async (req, res) => {
     console.log('\nIncoming request with the following params: \n METHOD: %s\n URL: %s\n BODY: %s\n', req.method, req.url, JSON.stringify(req.body))
 
-    const createdComment = await commentsActions.insertSingle(req.body)
+    const commentToInsert = req.body
 
-    res.send(createdComment);
+    // add Gravatar URL to the comment object
+    const emailHash = crypto.createHash('md5').update(commentToInsert.email.trim().toLowerCase()).digest("hex")
+    commentToInsert.userPicURL = 'https://www.gravatar.com/avatar/' + emailHash
+
+    const insertedComment = await commentsActions.insertSingle(commentToInsert)
+
+    res.send(insertedComment)
 })
 
 app.listen(config.httpServerPort, () => console.log(`Comments service is listening on port ${config.httpServerPort}!`))
