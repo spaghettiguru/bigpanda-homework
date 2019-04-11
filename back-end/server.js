c44dd393f9ea118996ea74e06698bbdb288adb6d
Oleg Yuzvik <yuzvik.oleg@gmail.com>
2019-04-11 22:12:30 +0300

    [SERVER] code formatting

--- back-end/server.js
+++ back-end/server.js
@@ -27,12 +27,16 @@ app.get('/', async (req, res) => {
 })
 
 app.post('/', async (req, res) => {
-    console.log('\nIncoming request with the following params: \n METHOD: %s\n URL: %s\n BODY: %s\n', req.method, req.url, JSON.stringify(req.body))
+    console.log('\nIncoming request with the following params: \n METHOD: %s\n URL: %s\n BODY: %s\n', 
+        req.method, req.url, JSON.stringify(req.body))
 
     const commentToInsert = req.body
 
-    // add Gravatar URL to the comment object
-    const emailHash = crypto.createHash('md5').update(commentToInsert.email.trim().toLowerCase()).digest("hex")
+    // add Gravatar URL to the comment object before inserting it to the db
+    const emailHash = crypto
+                            .createHash('md5')
+                            .update(commentToInsert.email.trim().toLowerCase())
+                            .digest("hex")
     commentToInsert.userPicURL = 'https://www.gravatar.com/avatar/' + emailHash
 
     const insertedComment = await commentsActions.insertSingle(commentToInsert)
